e96567227fef0e9c72217a4b9bd903d323366a9f
HetBhikadiya <hetbhikadiya2007@gmail.com>
2025-06-11 17:30:08 +0530

    Add files via upload
    Generated By Ai

---
+++ stocktrading/src/stocktrading/util/StockPrinter.java
@@ -0,0 +1,9 @@
+package stocktrading.util;
+
+public class StockPrinter {
+    public static void print(String message) {
+        System.out.println("+-----------------------------+");
+        System.out.println("| " + message + " |");
+        System.out.println("+-----------------------------+");
+    }
+}
